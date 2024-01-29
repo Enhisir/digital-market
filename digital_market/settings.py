db2960069b3512f3a3a4f97d6e634ef2d794e95e
Matvey <enhisir@localhost.localdomain>
2024-01-29 14:58:18 +0500

    подключил PostgreSQL

--- digital_market/settings.py
+++ digital_market/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'digital_market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'online-market',
+        'USER': 'postgres',
+        'PASSWORD': 'xdkess2004',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
